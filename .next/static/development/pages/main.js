34aa2bf39bbadd617f277f60926c0419522c42b7
homonnom <josojoso0702@gmail.com>
2020-04-12 23:31:24 +0900

    initial commit

---
+++ .next/static/development/pages/main.js
@@ -0,0 +1,104 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/main.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fmain&absolutePagePath=%2FUsers%2Fjackjoo%2FDocuments%2Fdaeun-portfolio%2Fpages%2Fmain.js!./":
+/*!***********************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fmain&absolutePagePath=%2FUsers%2Fjackjoo%2FDocuments%2Fdaeun-portfolio%2Fpages%2Fmain.js ***!
+  \***********************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/main", function() {
+      var mod = __webpack_require__(/*! ./pages/main.js */ "./pages/main.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/main.js */ "./pages/main.js", function() {
+          if(!next.router.components["/main"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/main.js */ "./pages/main.js")
+          next.router.update("/main", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_2adc2403d89adc16ead0 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_2adc2403d89adc16ead0 */ "dll-reference dll_2adc2403d89adc16ead0"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/main.js":
+/*!***********************!*\
+  !*** ./pages/main.js ***!
+  \***********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/jackjoo/Documents/daeun-portfolio/pages/main.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2,
+      columnNumber: 3
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3,
+      columnNumber: 5
+    }
+  }, "Main Page"), __jsx("p", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 5
+    }
+  }, "Welcome to Main page."));
+});
+
+/***/ }),
+
+/***/ 3:
+/*!***************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fmain&absolutePagePath=%2FUsers%2Fjackjoo%2FDocuments%2Fdaeun-portfolio%2Fpages%2Fmain.js ***!
+  \***************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fmain&absolutePagePath=%2FUsers%2Fjackjoo%2FDocuments%2Fdaeun-portfolio%2Fpages%2Fmain.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fmain&absolutePagePath=%2FUsers%2Fjackjoo%2FDocuments%2Fdaeun-portfolio%2Fpages%2Fmain.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_2adc2403d89adc16ead0":
+/*!*******************************************!*\
+  !*** external "dll_2adc2403d89adc16ead0" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_2adc2403d89adc16ead0;
+
+/***/ })
+
+},[[3,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=main.js.map
---
+++ pages/main.js
@@ -0,0 +1,6 @@
+export default () => (
+  <div>
+    <h1>Main Page</h1>
+    <p>Welcome to Main page.</p>
+  </div>
+);
